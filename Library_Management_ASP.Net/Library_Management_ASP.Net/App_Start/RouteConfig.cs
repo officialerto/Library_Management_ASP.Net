aeac5f5ba65c0c0cdafe3b57d8fc579e793517c1
Ertugrul <57365047+officialerto@users.noreply.github.com>
2023-03-30 19:28:09 +0300

    Library_Management_ASP.Net_LastUpdate_30.03.2023

--- Library_Management_ASP.Net/Library_Management_ASP.Net/App_Start/RouteConfig.cs
+++ Library_Management_ASP.Net/Library_Management_ASP.Net/App_Start/RouteConfig.cs
@@ -16,7 +16,7 @@ namespace Library_Management_ASP.Net
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+                defaults: new { controller = "Default", action = "Index", id = UrlParameter.Optional }
             );
         }
     }
